de3d6545bbf3402fd40a2a39bdb930dd06cc2fd7
XxArnauGxX <gilgilarnau@gmail.com>
2025-02-13 17:37:30 +0100

    Componente Footer en proceso

--- frontend/pages/Home.jsx
+++ frontend/pages/Home.jsx
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Header from '../components/Header.jsx';
+import Footer from '../components/Footer.jsx';
 
 function Home() {
     return (
         <div className='homeContainer'>
             <Header />
+            <Footer />
         </div>
     );
 }
