9c46800cf87a6af45604452595ddbf08d5262258
Arnau Gil Gil <162440885+XxArnauGxX@users.noreply.github.com>
2025-04-26 14:15:53 +0200

    feat: añadir estilos con css vanilla al componente Button

---
+++ src/components/ui/Button/Button.jsx
@@ -0,0 +1,44 @@
+import React, { forwardRef } from "react";
+import "./Button.css";
+
+/**
+ * Botón reutilizable
+ *
+ * Props:
+ * - children: contenido del botón
+ * - type: tipo de botón (button, submit, reset)
+ * - className: clases extra para personalización
+ * - disabled: deshabilitar el botón
+ * - onClick: función al hacer click
+ * - ...props: cualquier otro atributo estándar de <button>
+ */
+const Button = forwardRef(
+  (
+    {
+      children,
+      type = "button",
+      className = "",
+      disabled = false,
+      onClick,
+      ...props
+    },
+    ref
+  ) => (
+    <button
+      ref={ref}
+      type={type}
+      className={`button ${className}`}
+      disabled={disabled}
+      onClick={onClick}
+      aria-disabled={disabled}
+      tabIndex={disabled ? -1 : 0}
+      {...props}
+    >
+      {children}
+    </button>
+  )
+);
+
+Button.displayName = "Button";
+
+export default Button;
