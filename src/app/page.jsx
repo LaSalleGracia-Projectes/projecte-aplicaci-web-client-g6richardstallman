a997577aea9854464ae3877dc4fe8b4e81af54a7
Arnau Gil Gil <162440885+XxArnauGxX@users.noreply.github.com>
2025-04-27 21:38:01 +0200

    feat: añadir página principal

--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,12 +1,9 @@
-import Header from "../components/layout/Header";
+import Header from "../components/layout/Header/Header";
 
 export default function Home() {
   return (
     <>
       <Header />
-      <main className="p-8">
-        <h1 className="text-2xl font-bold mb-4">Bienvenido a Eventflix</h1>
-      </main>
     </>
   );
 }
