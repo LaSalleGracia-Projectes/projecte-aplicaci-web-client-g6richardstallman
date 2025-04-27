4ed9a2ea2f79b3f6de34b1412d997aaa966ff1d2
Arnau Gil Gil <162440885+XxArnauGxX@users.noreply.github.com>
2025-04-27 19:57:52 +0200

    feat: añadir layout para las páginas de login, register y reset-password

---
+++ src/app/auth/layout.jsx
@@ -0,0 +1,22 @@
+import Image from "next/image";
+import "./layout.css";
+
+export default function AuthLayout({ children }) {
+  return (
+    <div className="auth-layout-container">
+      <div className="auth-layout-image-panel">
+        <Image
+          src="/registerImage.jpg"
+          alt="Registro Eventflix"
+          fill
+          className="next-image"
+          priority
+        />
+        <div className="auth-layout-image-overlay" />
+      </div>
+      <div className="auth-layout-main">
+        <div className="auth-layout-content">{children}</div>
+      </div>
+    </div>
+  );
+}
